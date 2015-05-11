7ad7e18782e7ba7e00fde3220d50975e8538411c
Mohammad Nurdin <nurdinnorazanservices@gmail.com>
2015-05-11 16:25:39 +0800

    Update project setting.

---
+++ RevivalxSwiftAESCrypt/Model/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  RevivalxSwiftAESCrypt
+//
+//  Created by Mohammad Nurdin bin Norazan on 5/10/15.
+//  Copyright (c) 2015 Nurdin Norazan Services. All rights reserved.
+//
+
+import Foundation
